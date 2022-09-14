4c2629ed3f768b3c5833c4a5420508e79ce56cc0
khalidzein83 <khalid.zein1@gmail.com>
2022-09-14 19:31:44 -0400

    finished uploading all the data and the sort modal

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -5,10 +5,14 @@ const Cards = (props) => {
     return (
         <div className="cards">
             <div className="cards__card">
-                <img src={props.image} alt={props.name} />
+                <img
+                    className="cards__image"
+                    src={props.image}
+                    alt={props.name}
+                />
                 <p className="cards__brand">{props.brand}</p>
                 <p className="cards__name">{props.name}</p>
-                <p className="cards__price">$ {props.price}</p>
+                <p className="cards__price">${props.price}</p>
             </div>
         </div>
     );
